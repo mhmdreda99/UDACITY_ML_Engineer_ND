3c15f3075fc3759989e9ef80e6fe90d3d15abe28
mhmdreda99 <Moreda491999@gmail.com>
2021-02-03 18:05:54 +0200

    Create SW engineering folder and add 2nd coourse files

---
+++ 02-SW Engineering/OOP - 03 - code_gaussian_class/tempCodeRunnerFile.py
@@ -0,0 +1,31 @@
+ mu = self.mean
+        sigma = self.stdev
+
+        min_range = min(self.data)
+        max_range = max(self.data)
+        
+         # calculates the interval between x values
+        interval = 1.0 * (max_range - min_range) / n_spaces
+
+        x = []
+        y = []
+        
+        # calculate the x values to visualize
+        for i in range(n_spaces):
+            tmp = min_range + interval*i
+            x.append(tmp)
+            y.append(self.pdf(tmp))
+
+        # make the plots
+        fig, axes = plt.subplots(2,sharex=True)
+        fig.subplots_adjust(hspace=.5)
+        axes[0].hist(self.data, density=True)
+        axes[0].set_title('Normed Histogram of Data')
+        axes[0].set_ylabel('Density')
+
+        axes[1].plot(x, y)
+        axes[1].set_title('Normal Distribution for \n Sample Mean and Sample Standard Deviation')
+        axes[0].set_ylabel('Density')
+        plt.show()
+
+        return x, y
